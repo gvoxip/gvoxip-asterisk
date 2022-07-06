034e45ac30f26534bbb4111cdcb2d27b56d963be
bobymayky <bobymayky@gmail.com>
2022-07-06 13:55:43 -0400

    Echo sql cdr 8

--- libs/paloSantoCDR.class.php
+++ libs/paloSantoCDR.class.php
@@ -219,7 +219,7 @@ SQL_COND_EXTENSION;
         
         // Construir fragmento completo de sentencia SQL
         $where = array(implode(' AND ', $condSQL), $paramSQL);
-        if ($where[0] != '') $where[0] = 'WHERE '.$where[0];
+        //if ($where[0] != '') $where[0] = 'WHERE '.$where[0];
        // echo $where[0];
         return $where;
     }
